23a53536bf72525ccd6a405928abf62641d7b5f6
shaeekkhan <96725497+shaeekkhan@users.noreply.github.com>
2023-12-20 21:39:22 +0600

    Footer form and products nav list

--- utils/footer_po.py
+++ utils/footer_po.py
@@ -1,7 +1,6 @@
 import time
 
 from selenium.common.exceptions import TimeoutException
-# from selenium.webdriver import ActionChains
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
