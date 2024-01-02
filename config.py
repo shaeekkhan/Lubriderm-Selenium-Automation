a519b1e2084e553a9b4b8ee9beafd060c5c75271
shaeekkhan <96725497+shaeekkhan@users.noreply.github.com>
2024-01-02 16:16:49 +0600

    Completed the PDP pagee

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
 class Config:
     base_url = "https://www.lubriderm.com/"
     pdp_daily_moisture_lotion = "https://www.lubriderm.com/products/daily-moisture-lotion-fragrance-free"
+    plp_all_products = "https://www.lubriderm.com/products"
+
---
+++ selectors/plp_all_products_selectors.py
@@ -0,0 +1,18 @@
+class PlpAllProducts:
+    bottom_pop_up_close = '//*[@id="onetrust-close-btn-container"]/button'
+    banner_header = '//*[@id="__next"]/main/div[2]/div[2]/div/h1'
+    banner_sub_copy = '//*[@id="__next"]/main/div[2]/div[2]/div/p'
+    banner_image = '//*[@id="__next"]/main/div[2]/div[1]/img'
+    products_card_first_row = '//*[@id="__next"]/main/div[3]/div[1]/div[1]'
+    product1_image = '//*[@id="__next"]/main/div[3]/div[1]/div[1]/div[1]/a'
+    first_eye_brow_tag = '//*[@id="__next"]/main/div[3]/div[1]/div[1]/div[2]/p'
+    product1_name = '//*[@id="__next"]/main/div[3]/div[1]/div[1]/div[2]/a'
+    product1_buy_now_cta = '//*[@id="__next"]/main/div[3]/div[1]/div[1]/div[3]/div[2]/div'
+    price_spider_pop_up1 = "body > div.ps-container.ps-lightbox.ps-6462c37d7d0774004fa25168.ps-open > div"
+    price_spider_pop_up1_close = "body > div.ps-container.ps-lightbox.ps-6462c37d7d0774004fa25168.ps-open > div > span"
+    product2_image = '//*[@id="__next"]/main/div[3]/div[1]/div[2]/div[1]/a'
+    second_eye_brow_tag = '//*[@id="__next"]/main/div[3]/div[1]/div[2]/div[2]/p'
+    product2_name = '//*[@id="__next"]/main/div[3]/div[1]/div[2]/div[2]/a'
+    product2_buy_now_cta = '//*[@id="__next"]/main/div[3]/div[1]/div[2]/div[3]/div[2]/div'
+    price_spider_pop_up2 = "body > div.ps-container.ps-lightbox.ps-6462c37d7d0774004fa25168.ps-open > div"
+    price_spider_pop_up2_close = "body > div.ps-container.ps-lightbox.ps-6462c37d7d0774004fa25168.ps-open > div > span"
