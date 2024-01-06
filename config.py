755304f42bba9cba15ab0419e3fb1ecb9426f7c5
shaeekkhan <96725497+shaeekkhan@users.noreply.github.com>
2024-01-06 21:39:18 +0600

    Started with the FAq MODULE

--- config.py
+++ config.py
@@ -2,4 +2,4 @@ class Config:
     base_url = "https://www.lubriderm.com/"
     pdp_daily_moisture_lotion = "https://www.lubriderm.com/products/daily-moisture-lotion-fragrance-free"
     plp_all_products = "https://www.lubriderm.com/products"
-
+    faq = "https://www.lubriderm.com/frequently-asked-questions"
